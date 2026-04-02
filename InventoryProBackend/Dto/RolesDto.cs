004c73dbac73d83e41a6b511c2172a5efd4f441e
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-01 20:07:46 -0600

    se crea el dto que mostrara los parametros obligatorios en el Crud

---
+++ InventoryProBackend/Dto/RolesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryProBackend.Dto
+{
+    public class RolesDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ InventoryProBackend/Dto/UsersDto.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryProBackend.Dto
+{
+    public class UsersDto
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string PasswordHash { get; set; }
+        public string RoleId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
