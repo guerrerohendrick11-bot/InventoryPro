7a44505a00e6d663c8581d3232f9e15d0bb7c48e
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-01 20:06:41 -0600

    Se crea el DbContext para añadir los models y crear las tablas

---
+++ InventoryProBackend/Data/ContextDb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryProBackend.Data
+{
+    public class ContextDb: DbContext
+    {
+        public ContextDb(DbContextOptions<ContextDb>options): base(options)
+        {
+            
+
+        }
+         public DbSet<Models.Categories> Categories { get; set; }
+         public DbSet<Models.Customers> Customers{ get; set; }
+         public DbSet<Models.Products> Products { get; set; }
+         public DbSet<Models.Roles> Roles { get; set; }
+         public DbSet<Models.SaleDetails> SaleDetails { get; set; }
+         public DbSet<Models.Sales> Sales { get; set; }
+         public DbSet<Models.Users> Users { get; set; }
+       
+    }
+}
