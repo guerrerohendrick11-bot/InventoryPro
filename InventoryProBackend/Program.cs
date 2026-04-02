bcc065b6d8a4ce2f93eddc2326c0fcd3aaf56c1f
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-01 19:51:45 -0600

    se modifican los archivos del program se agrega la conexion string el swagger y la conexion al controller

--- InventoryProBackend/Program.cs
+++ InventoryProBackend/Program.cs
@@ -1,3 +1,8 @@
+using InventoryProBackend.Data;
+using InventoryProBackend.InterfaceService;
+using InventoryProBackend.Service;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +11,27 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ContextDb>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<ICategori, CategoriService>();
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
