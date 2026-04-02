0217cf0e174758f60ef84b7d6b64d5efe3cafa15
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-01 20:09:57 -0600

    se crean las interfaces que se implementaran en los Services para mostrar los parametros del swagger

---
+++ InventoryProBackend/InterfaceService/Icustomer.cs
@@ -0,0 +1,13 @@
+﻿using InventoryProBackend.Dto;
+
+namespace InventoryProBackend.InterfaceService
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<CustomerDto>> GetAllAsync();
+        Task<CustomerDto> GetByIdAsync(int id);
+        Task<CustomerDto> CreateAsync(CustomerDto dto);
+        Task<bool> UpdateAsync(int id, CustomerDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
