735987f150751f9c4e0654836faeef4de664b2c4
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-03 14:36:47 -0600

    Se agrega la interfaz de AuthService

---
+++ InventoryProBackend/InterfaceService/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using InventoryProBackend.Dto.Login;
+
+namespace InventoryProBackend.InterfaceService
+{
+    public interface IAuthService
+    {
+        Task<string> LoginAsync(LoginDto dto);
+    }
+}
