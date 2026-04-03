7e3ee7e7f97e9880b4cf67502ccd052aba00a8a2
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-03 14:31:19 -0600

    se agrega el controller de Auth que crea el tokend para usuarios

---
+++ InventoryProBackend/Controller/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using InventoryProBackend.Dto;
+using InventoryProBackend.Dto.Login;
+using InventoryProBackend.InterfaceService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryProBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            try
+            {
+                var token = await _authService.LoginAsync(dto);
+                return Ok(new { token });
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
+        }
+    }
+}
