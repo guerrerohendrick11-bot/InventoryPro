10d6d99d47542308c6f3462b8f75f384fe256dc2
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-04 20:34:35 -0600

    se modifican estilos y el html para que el usuario tenga una mejor vista de la app

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // 👈 IMPORT CORRECTO
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(), // 👈 AQUÍ ESTÁ LA CLAVE
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay())
   ]
 };
