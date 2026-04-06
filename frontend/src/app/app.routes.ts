93702f6672640beb4c2d1ab19185d395fb242744
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-06 15:57:39 -0600

    Se modifican las rutas para mostrar los services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/login/login.component';
+import { ListComponent } from './modules/products/list/list';
+import { FormComponent } from './modules/products/form/form';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ListComponent },
+  { path: 'products/create', component: FormComponent },
+  { path: 'products/edit/:id', component: FormComponent }
 ];
