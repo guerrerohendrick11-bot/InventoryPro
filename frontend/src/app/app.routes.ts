6d70a3df2066bebce9d383fc6dfb8b7107018935
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-07 14:02:39 -0600

    chore: actualizar rutas de la aplicación

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/login/login.component';
 import { ListComponent } from './modules/products/list/list';
 import { FormComponent } from './modules/products/form/form';
+import { NewSaleComponent } from './modules/sales/new-sale/new-sale';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'products', component: ListComponent },
   { path: 'products/create', component: FormComponent },
-  { path: 'products/edit/:id', component: FormComponent }
+  { path: 'products/edit/:id', component: FormComponent },
+  { path: 'sales/create', component: NewSaleComponent }
 ];
