5d0694e9c444f78802ce53599ef472674906846e
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-07 14:05:13 -0600

    feat: implementar servicio de ventas

---
+++ frontend/src/app/core/services/sale.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SaleService {
+
+  private apiUrl = 'https://localhost:7265/api/Sales';
+
+  constructor(private http: HttpClient) {}
+
+  private getAuthHeaders() {
+    const token = localStorage.getItem('token');
+
+    return {
+      Authorization: `Bearer ${token}`
+    };
+  }
+
+  create(sale: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, sale, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl, {
+      headers: this.getAuthHeaders()
+    });
+  }
+}
