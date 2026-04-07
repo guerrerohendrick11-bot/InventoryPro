cb77e3d6ac736136b90ee2a8d29e328ff86bcbed
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-07 13:56:27 -0600

    implement product list with API integration

--- frontend/src/app/modules/products/list/list.ts
+++ frontend/src/app/modules/products/list/list.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../core/services/product.service';
 
 @Component({
-  selector: 'app-list',
-  imports: [],
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './list.html',
-  styleUrl: './list.css',
+  styleUrls: ['./list.css']
 })
-export class List {
+export class ListComponent implements OnInit {
 
+  products: any[] = [];
+
+  searchName: string = '';
+  searchCategoryId: number | null = null;
+
+  constructor(
+    private productService: ProductService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getAll(this.searchName, this.searchCategoryId).subscribe({
+      next: (data: any[]) => {
+        console.log('PRODUCTOS:', data);
+        this.products = data;
+        this.cdr.detectChanges();
+      },
+      error: (err: any) => {
+        console.error('ERROR AL CARGAR PRODUCTOS:', err);
+      }
+    });
+  }
+
+  searchProducts(): void {
+    this.getProducts();
+  }
+
+  clearFilters(): void {
+    this.searchName = '';
+    this.searchCategoryId = null;
+    this.getProducts();
+  }
 }
