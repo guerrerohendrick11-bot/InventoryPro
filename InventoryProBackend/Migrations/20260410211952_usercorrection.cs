95c930c144107df97297e0211c020f546d29e42e
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-04-10 15:30:39 -0600

    se agrega una migracion para correjir una entidad

---
+++ InventoryProBackend/Migrations/20260410211952_usercorrection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryProBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class usercorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsuerId",
+                table: "Sales");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsuerId",
+                table: "Sales",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
